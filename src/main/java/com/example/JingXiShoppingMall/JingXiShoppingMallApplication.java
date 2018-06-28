a202b38d03946c4397c4c3b5979109377ace4b86
xuexue <1047504737@qq.com>
2018-06-28 12:21:02 +0800

    first commit

---
+++ src/main/java/com/example/JingXiShoppingMall/JingXiShoppingMallApplication.java
@@ -0,0 +1,12 @@
+package com.example.JingXiShoppingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingXiShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingXiShoppingMallApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/JingXiShoppingMall/JingXiShoppingMallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.JingXiShoppingMall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JingXiShoppingMallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
