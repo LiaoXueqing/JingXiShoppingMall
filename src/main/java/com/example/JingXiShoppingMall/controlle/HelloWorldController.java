de3a003c5008ef3b11fec09827b0acb4c1c903d9
xuexue <1047504737@qq.com>
2018-06-30 21:43:28 +0800

    READ.md and example.png

--- src/main/java/com/example/JingXiShoppingMall/controlle/HelloWorldController.java
+++ src/main/java/com/example/JingXiShoppingMall/controlle/HelloWorldController.java
@@ -1,4 +1,25 @@
 package com.example.JingXiShoppingMall.controlle;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("")
 public class HelloWorldController {
+    @GetMapping("index")
+    public String helloWorld() {
+        System.out.println("HelloWorldController");
+        return "/index";
+    }
+    @GetMapping("welcome")
+    public String welcome() {
+        System.out.println("welcome");
+        return "/welcome";
+    }
 }
